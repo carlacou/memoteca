fde2c608e4ca6ef129461dfffec799b596737f78
Carla F C P Alves <carlacoutinhopontes@gmail.com>
2023-06-05 21:20:53 -0300

    componente editar

--- memoteca/src/app/app-routing.module.ts
+++ memoteca/src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { CriarPensamentoComponent } from './componentes/pensamentos/criar-pensamento/criar-pensamento.component';
 import { ListarPensamentoComponent } from './componentes/pensamentos/listar-pensamento/listar-pensamento.component';
 import { ExcluirPensamentosComponent } from './componentes/pensamentos/excluir-pensamentos/excluir-pensamentos.component';
+import { EditarPensamentoComponent } from './componentes/pensamentos/editar-pensamento/editar-pensamento.component';
 
 const routes: Routes = [
   {
@@ -21,6 +22,10 @@ const routes: Routes = [
   {
     path: 'pensamentos/excluirPensamento/:id',
     component: ExcluirPensamentosComponent
+  },
+  {
+    path:'pensamentos/editarPensamento/:id',
+    component:EditarPensamentoComponent
   }
 ];
 
